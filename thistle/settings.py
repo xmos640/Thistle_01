e2ad9a6d945a81c43e52a153c064557a3b0fa0df
xmos640 <udaynambissan@gmail.com>
2023-09-19 19:42:37 +0530

    adad

--- thistle/settings.py
+++ thistle/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['thistle01-production.up.railway.app','127.0.0.1']
 # Application definition
 
-CSRF_TRUSTED_ORIGINS = ['https://thistle01-production.up.railway.app/','https://*.127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['https://thistle01-production.up.railway.app/','https://*.127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
