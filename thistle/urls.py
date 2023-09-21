03c0732e45cf6d597653760e573c44bde954a647
xmos640 <udaynambissan@gmail.com>
2023-09-21 14:45:48 +0530

    auth

--- thistle/urls.py
+++ thistle/urls.py
@@ -22,5 +22,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
-    
-] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path(r'^oauth/', include('social_django.urls', namespace='social'))
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static (settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
